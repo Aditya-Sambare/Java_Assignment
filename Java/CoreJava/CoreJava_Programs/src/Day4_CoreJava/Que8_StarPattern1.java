37fca264c3352d34321bc0a31d9d256a58a9c6ba
Aditya-Sambare <adityasambare123@gmail.com>
2022-07-22 16:01:48 +0530

    Day4_CoreJava

--- Java/CoreJava/CoreJava_Programs/src/Day4_CoreJava/Que8_StarPattern1.java
+++ Java/CoreJava/CoreJava_Programs/src/Day4_CoreJava/Que8_StarPattern1.java
@@ -1,12 +1,19 @@
 package Day4_CoreJava;
 class print_Pattern{
     public static void display(){
-        
-
-
+        int b = -1;
+        for(int i = 1 ; i <= 9 ; i++){
+            int a=i<=5?b++:b--;
+            for (int j = 1; j <= 9 ; j++){
+                if(j >= 5 - b && j <= 5 + b)
+                    System.out.print("*");
+                else
+                    System.out.print(" ");
+            }
+            System.out.println();
+        }
     }
 }
-
 public class Que8_StarPattern1 {
     public static void main(String[] args) {
         print_Pattern.display();
