aa6c5e421f787c93ed901ad14ee0d8e944ed2f4c
Aditya-Sambare <adityasambare123@gmail.com>
2022-07-25 14:23:35 +0530

    updates

--- Java/CoreJava/CoreJava_Programs/src/Day5_CoreJava/Que3_2.java
+++ Java/CoreJava/CoreJava_Programs/src/Day5_CoreJava/Que3_2.java
@@ -1,20 +1,20 @@
 package Day5_CoreJava;
 class Static_Demo1 {
-//    //static method
-//    public static void addition(int a, int b) {
-//        int c = a + b;
-//        System.out.println("The sum is: " + c);
-//    }
-//
-//    //non-static method
-//    public static void addition(int a, int b) {
-//        int c = a + b;
-//        System.out.println("The sum is: " + c);
-//    }
+    //static method
+    public static void addition(int a, int b) {
+        int c = a + b;
+        System.out.println("The sum is: " + c);
+    }
 }
-public class Que3_2 {
+class Static_Demo2 extends Static_Demo1 {
+    public static void addition(int a, int b) {
+        int c = a + b;
+        System.out.println("The sum is: " + c);
+    }
+}
+    public class Que3_2 {
     public static void main(String[] args) {
-//        Static_Demo1.addition(10,20);
+//
         System.out.println("we can not override the static method");
 
     }
