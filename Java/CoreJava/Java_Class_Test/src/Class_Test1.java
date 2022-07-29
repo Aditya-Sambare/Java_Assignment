b5d050ab443597c279073eeaff07bc24269a900a
Aditya-Sambare <adityasambare123@gmail.com>
2022-07-29 11:44:59 +0530

    core java class test

---
+++ Java/CoreJava/Java_Class_Test/src/Class_Test1.java
@@ -0,0 +1,24 @@
+import java.util.Arrays;
+class Array1{
+    public void main() {
+        int[] arr = {2, 15, 1, 100, 70};
+        System.out.print("Array is : ");
+        for (int n : arr) {
+            System.out.print(n + " ");
+        }
+        System.out.println();
+        Arrays.sort(arr);
+        int largest = 1;
+        System.out.println(+largest+" largest no is : "+arr[arr.length - largest]);
+
+        int whichLargest = 2;
+        System.out.print(whichLargest + " largest no is : " + arr[arr.length - whichLargest]);
+    }
+}
+public class Class_Test1 {
+    public static void main(String[] args) {
+        Array1 array1 = new Array1();
+        array1.main();
+    }
+}
+
